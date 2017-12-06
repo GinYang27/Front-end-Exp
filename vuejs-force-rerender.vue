cb3eea3fcee8e2399c842bacaa428912e4ed67de
GinYang27 <gin.yang17@gmail.com>
2017-12-06 14:51:43 -0600

    Create vuejs-force-rerender.vue

---
+++ vuejs-force-rerender.vue
@@ -0,0 +1,7 @@
+<template>
+  <comment-post :commentandreplies="commentandreplies" @updateComment = "updateFlag++" :key="updateFlag"></comment-post>
+</template>
+
+rerenderComments() {
+  this.updateFlag ++;
+},
